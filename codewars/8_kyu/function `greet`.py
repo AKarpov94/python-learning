bc7ac9967e8d3141893d1f46e76bb020c080fb28
Art <artem.karpov.22.02.1994@bk.ru>
2026-09-02 20:51:06 +0300

    Write a function `greet` that returns "hello world!"

---
+++ codewars/8_kyu/function `greet`.py
@@ -0,0 +1,6 @@
+# Write a function `greet` that returns "hello world!"
+def greet():
+    return ("hello world")
+#Того что выше достаточно, ниже просто роверил работает функция или нет
+r=greet()
+print(r)
