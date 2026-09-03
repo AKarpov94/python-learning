8061720af44c3f1c522c9703be1b5717a501dc36
Art <artem.karpov.22.02.1994@bk.ru>
2026-09-03 15:37:48 +0300

    Find the smallest integer in the array

---
+++ codewars/8_kyu/Find the smallest integer in the array.py
@@ -0,0 +1,13 @@
+def find_smallest_int(arr):
+    arr.sort()
+    return arr[0]
+
+#def findSmallestInt(arr):
+#   return min(arr)
+
+#def findSmallestInt(arr):
+ #   min = arr[0]
+  #  for item in arr:
+        if min > item:
+            min = item
+   # return min
