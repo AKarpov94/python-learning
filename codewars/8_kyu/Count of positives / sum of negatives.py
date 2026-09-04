8488e3df4ae632965639c61da8180595e167408e
Art <artem.karpov.22.02.1994@bk.ru>
2026-09-04 08:44:12 +0300

    Count of positives / sum of negatives

---
+++ codewars/8_kyu/Count of positives / sum of negatives.py
@@ -0,0 +1,11 @@
+def count_positives_sum_negatives(arr):
+    if arr == []:
+        return []
+    a = 0
+    b = 0
+    for i in arr:
+        if i > 0:
+            a = a + 1
+        if i < 0:
+            b = b + i
+    return [a,b]
